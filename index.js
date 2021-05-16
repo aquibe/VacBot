f9cc86a35cadeb6bc8b6e2d2506036295836ba3a
Aquibe <60189864+aquibe@users.noreply.github.com>
2021-05-16 18:55:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+const Discord = require('discord.js')
+const client= new Discord.Client()
+const https = require('https')
+const express=require('express')
+const app=express()
+
+app.get('/api/kerala',(req,res)=>{
+    https.get('https://cdn-api.co-vin.in/api/v2/admin/location/districts/17',(resp)=>{
+        resp.on('data',(data)=>{
+            res.send(JSON.parse(data))
+        })
+    })
+   
+})
+
+client.on('ready',(msg)=>{
+    console.log('logged in as '+client.user.tag)
+})
+
+client.on('message',(msg)=>{
+    console.log('username : '+msg.author.username)
+    console.log('channel : '+msg.channel)
+    console.log('content : '+msg.content)
+    console.log('------------------------------------------')
+    if(!msg.author.bot){
+        const message=msg.content.toLowerCase();
+        switch(message){
+            case 'hello':msg.reply('hello')
+                        break
+            case 'about':msg.reply('just a test bot')
+                        break
+            case 'bye':msg.reply('bye')
+                        break
+
+            default:msg.channel.send('invalid command')
+        }
+    }
+})
+
+client.login(process.env.TOKEN)
+
+app.listen(3000)
